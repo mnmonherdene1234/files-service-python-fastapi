ba96a8f756ced49de9527c1d36a571a02169c808
mnmonherdene1234 <mnmonherdene1234@gmail.com>
2023-11-08 13:23:31 +0800

    file delete done

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
-import hashlib
+import datetime
 import os
-import zlib
 from pathlib import Path
 
+from cryptography.fernet import Fernet
 from fastapi import FastAPI
 from fastapi import File, UploadFile
-
-from cryptography.fernet import Fernet
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+
+folder_name = "./files"
+if not os.path.exists(folder_name):
+    os.makedirs(folder_name)
 
 app = FastAPI()
 app.mount("/files", StaticFiles(directory="files"), name="files")
 
-folder_name = "files"
-
-if not os.path.exists(folder_name):
-    os.makedirs(folder_name)
-
 
 @app.get("/")
-def read_root():
-    return {"message": "Files Service running!"}
+def info():
+    return {"message": "Files Service running!", "date": datetime.datetime.now()}
 
 
 @app.post('/upload')
 def upload(file: UploadFile = File(...)):
-    random_name = os.urandom(32).hex()
-    filepath = f"files/{random_name}{Path(file.filename).suffix}"
+    random_filename = os.urandom(32).hex()
+    file_extension = Path(file.filename).suffix
+    filename = f"{random_filename}{file_extension}"
+    filepath = f"files/{filename}"
 
     key = Fernet.generate_key()
     cipher_suite = Fernet(key)
@@ -38,18 +38,25 @@ def upload(file: UploadFile = File(...)):
 
         with open(filepath, 'wb') as f:
             f.write(encrypted_content)
-    except Exception:
-        return {"message": "There was an error uploading the file"}
+    except Exception as error:
+        return {"message": "There was an error uploading the file", "error": str(error)}
     finally:
         file.file.close()
 
-    compressed_filepath = f'{filepath}.z'
+    return {"filename": filename, "key": key}
 
-    with open(filepath, 'rb') as f:
-        compressed_data = zlib.compress(f.read(), level=9)
-        with open(compressed_filepath, 'wb') as ff:
-            ff.write(compressed_data)
 
-    os.remove(filepath)
+class Delete(BaseModel):
+    filename: str
 
-    return {"filename": random_name, "key": key}
+
+@app.delete('/delete-file')
+def delete_file(delete: Delete):
+    filepath = f'files/{delete.filename}'
+
+    try:
+        os.remove(filepath)
+    except Exception as error:
+        return {"message": str(error), "filename": delete.filename}
+
+    return {"message": "OK", "filename": delete.filename}
